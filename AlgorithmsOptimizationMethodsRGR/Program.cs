34ff37529f0ed5cece255c9071ba1dc7e31a8834
Константин <konstkonst55@mail.ru>
2026-04-29 15:01:32 +0700

    Добавьте файлы проекта.

---
+++ AlgorithmsOptimizationMethodsRGR/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsOptimizationMethodsRGR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
